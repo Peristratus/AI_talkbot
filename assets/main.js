82c6146c5c4841981e0b3df786bba2be08d1b2a3
Peristratus <testcorp24@gmail.com>
2021-02-13 20:42:16 +0000

    adding the code for the chat text container human and AI bots

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,33 @@
 const voice = document.querySelector(".voice");
-const voice2text = docdocument.querySelector(".voice2text");
+const voice2text = document.querySelector(".voice2text");
 
-const speeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recorder = new SpeechRecognition();
+
+function addHumanText(text){
+
+    const chatContainer = document.createElement("div");
+    chatContainer.classList.add("chat-container");
+
+    const chatBox = document.createElement("p");
+    chatBox.classList.add("voice2text");
+}
+
+recorder.onstart =() => {
+    console.log('Voice activated');
+};
+
+recorder.onresult = (event) => {
+//console.log(event);
+
+const resultIndex = event.resultIndex;
+const transcript = event.results[resultIndex][0].transcript;
+voice2text.textContent = transcript;
+
+};
+
+voice.addEventListener('click', () =>{
+recorder.start();
+
+})
+
