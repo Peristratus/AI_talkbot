d6e48a04bf1fccf78c6ae68e138dc76043a3cfd6
Peristratus <testcorp24@gmail.com>
2021-02-14 04:08:37 +0000

    adding the logic if statement to the conversation between bot and human

--- assets/main.js
+++ assets/main.js
@@ -43,13 +43,17 @@ function botVoice(message){
        speech.text = "I am fine, thanks. How are you ?";
      }  
 
-      if (message.includes('hello')) {
+    if (message.includes('hi')) {
        speech.text = "I am fine, thanks. How are you ?";
      }  
 
-   if (message.includes('fine')) {
+    if (message.includes('hello')) {
+       speech.text = "I am fine, thanks. How are you ?";
+     }  
+
+    if (message.includes('fine')) {
        speech.text = "Nice to hear that, how can i assist you today ?";
-   }
+    }
 
     if (message.includes('sick')) {
        speech.text = "what is wrong with you ?";
@@ -57,7 +61,35 @@ function botVoice(message){
 
    if (message.includes('temperature')) {
        speech.text = "what is your temperature ?";
-   }
+    }
+
+    if (message.includes('40 Celsius')) {
+           speech.text = "That is a high temperature, do you have flu like symptoms, if you do then say I do or say no ?";
+       }
+
+    if (message.includes('I do')){
+               speech.text = "You may have COVID 19, please go to your nearest emergency hospital, What is your first and last name ?"
+        }
+    if (message.includes('Jonathan Martin')) {
+           speech.text = "I see you are a member and your doctor is Jane Korban, I will inform her of your situation, please go to the nearest hospital !";
+       }
+    if (message.includes('no')){
+               speech.text = " That's good, will you like me to do something else for you today ? If you want me to book an appointment with your doctor say your first and last name, or say thank you to end this conversation ?";
+        }
+
+    if (message.includes('Philip Johnson')){
+               speech.text = " What is your date of birth ? Please say Day, Month and Year so I can pull up your records";
+        }
+    
+    if (message.includes('18th September 1978')){
+               speech.text = " You are a member your doctor's name is Jane Amber, she will be able to see you on the 20th of February 2021 at 15:00";
+        }
+    
+    if (message.includes('thank you')){
+               speech.text = "Thank you have a nice day and keep safe, always wear a mask and wash your hands, your well beign is important to us?"
+        }
+
+    
 
    speech.volume = 1;
    speech.rate = 1;
